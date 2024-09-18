9294fbc260261e5c1ed41ac65c32f5cc065e5204
Shashank Jaiswal <shashankjaiswal266@gmail.com>
2024-09-19 02:44:36 +0530

    first

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-# TODO your deploy script implementation...
+sudo apt-get update
+sudo apt-get install -y python3.12-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python app.py
